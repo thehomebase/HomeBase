8c45f42d21356e2a7b03a02998424f0d11e97ce6
joelsellsstuff <36894477-joelsellsstuff@users.noreply.replit.com>
2025-02-09 02:58:16 +0000

    Agent query: Is the Homebase logo now visible in the top left corner of the application?
    Update Homebase logo image
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/b8d2ac0e-a0a3-42a7-956e-d8ac1b5a5ca1/77047119-6143-4b81-a16f-8f5d3817bd31.jpg

--- client/src/components/ui/logo.tsx
+++ client/src/components/ui/logo.tsx
@@ -4,7 +4,7 @@ export function Logo({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
   return (
     <div className={`flex items-center ${className}`} {...props}>
       <img
-        src="/homebase-logo.png"
+        src="/homebaselogo.png"
         alt="Homebase Logo"
         className="h-10 w-auto"
         style={{ minWidth: '32px' }}
