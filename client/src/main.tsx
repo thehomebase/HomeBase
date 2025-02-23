8d692535e6e15fcd988ffb17cd2a45fb3b98d61e
Homebase <thehomebase.io@gmail.com>
2025-02-23 00:34:45 +0000

    Assistant checkpoint: Clean up main.tsx file
    Assistant generated file changes:
    - client/src/main.tsx: Clean up duplicate imports and declarations
    
    ---
    
    User prompt:
    
    error

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,3 @@
-
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-createRoot(document.getElementById('root')!).render(<App />);
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
